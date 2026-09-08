e764a325c7bca03149fb091371d7ebd11bfad3ee
wlakunn94-pixel <wlakunn94@gmail.com>
2026-09-09 01:44:45 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,7 +3,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/fox_X6885.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_X6885-eng \
+    omni_X6885-user-eng \
     omni_X6885-userdebug \
-    fox_X6885-eng \
+    fox_X6885-user-eng \
     fox_X6885-userdebug
